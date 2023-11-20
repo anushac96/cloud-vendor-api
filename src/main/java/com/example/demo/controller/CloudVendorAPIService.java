179e5b62b05cee4c6cc34db95a2fc8bade0c8764
anushac96 <anushac96@gmail.com>
2023-11-19 22:36:56 -0800

    created createCloudVendorDetails()

--- src/main/java/com/example/demo/controller/CloudVendorAPIService.java
+++ src/main/java/com/example/demo/controller/CloudVendorAPIService.java
@@ -1,6 +1,10 @@
 package com.example.demo.controller;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +20,12 @@ public class CloudVendorAPIService {
 	public CloudVendor getCloudVendorDetails(String vendorId) {
 		
 		return cloudVendor;
+	}
+	
+	@PostMapping
+	public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+		this.cloudVendor = cloudVendor;
+		return "Cloud Vendor created!!!";
+		
 	}
 }
