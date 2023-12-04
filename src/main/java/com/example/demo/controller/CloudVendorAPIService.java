ab383a3d41648f58b759d026b233c737121601d8
anushac96 <anushac96@gmail.com>
2023-12-04 14:28:50 -0800

    added put and delete method

--- src/main/java/com/example/demo/controller/CloudVendorAPIService.java
+++ src/main/java/com/example/demo/controller/CloudVendorAPIService.java
@@ -1,9 +1,9 @@
 package com.example.demo.controller;
 
-import javax.annotation.PostConstruct;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,21 +11,31 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.model.CloudVendor;
 
 @RestController
-@RequestMapping("/getcloudvendor")
+@RequestMapping("/cloudvendor")
 public class CloudVendorAPIService {
 	
 	CloudVendor cloudVendor;
 	
 	@GetMapping("{verdorId}")
-	public CloudVendor getCloudVendorDetails(String vendorId) {
-		
+	public CloudVendor getCloudVendorDetails(String vendorId) {	
 		return cloudVendor;
 	}
 	
 	@PostMapping
 	public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
 		this.cloudVendor = cloudVendor;
-		return "Cloud Vendor created!!!";
-		
+		return "Cloud Vendor created!!!";		
+	}
+	
+	@PutMapping
+	public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+		this.cloudVendor = cloudVendor;
+		return "Cloud Vendor updated!!!";
+	}
+	
+	@DeleteMapping("vendorId")
+	public String deleteCloudVendorDetail(String vendorId) {
+		this.cloudVendor = null;
+		return "Cloud vendor deleted";
 	}
 }
